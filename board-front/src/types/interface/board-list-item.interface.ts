d25a9a63abb007510089d851255e329e67ce986f
Dayoung <pdayoung0402@hufs.ac.kr>
2024-11-04 18:11:37 +0900

    11번 영상 찐 최종 혜원이한테 맞음

--- board-front/src/types/interface/board-list-item.interface.ts
+++ board-front/src/types/interface/board-list-item.interface.ts
@@ -1,6 +1,6 @@
 export default interface BoardListItem {
 
-    // 특정 유저 게시물 리스트 -> 우리거에서는 유저 게시물 리스트가 필요없는뎅 BoardListItem이거가 계속 필요할 거 같아서 넣어놈
+    // 특정 유저 게시물 리스트임! -> 우리거에서는 유저 게시물 리스트가 필요없는뎅 BoardListItem이거가 계속 필요할 거 같아서 넣어놈
     boardNumber:number;
     title: string;
     content: string;
