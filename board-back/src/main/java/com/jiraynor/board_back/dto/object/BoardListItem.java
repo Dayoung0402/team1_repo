24743b395a32c70cd5a14dfe3e53c8dc0c9649a3
jiwoo1020 <dlwldn041020@gmail.com>
2024-11-05 21:35:00 +0900

    프론트 코드 작성

--- board-back/src/main/java/com/jiraynor/board_back/dto/object/BoardListItem.java
+++ board-back/src/main/java/com/jiraynor/board_back/dto/object/BoardListItem.java
@@ -3,9 +3,9 @@ package com.jiraynor.board_back.dto.object;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 // 검색 게시물 리스트(혹시 몰라서 작성!)
+
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
