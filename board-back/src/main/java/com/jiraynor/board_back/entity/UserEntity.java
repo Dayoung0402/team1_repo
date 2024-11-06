515a021838a674e1801413e5c5264c0ad85e54bb
Dayoung <pdayoung0402@hufs.ac.kr>
2024-11-06 22:31:35 +0900

    24번 영상 중간 커밋

--- board-back/src/main/java/com/jiraynor/board_back/entity/UserEntity.java
+++ board-back/src/main/java/com/jiraynor/board_back/entity/UserEntity.java
@@ -2,6 +2,8 @@ package com.jiraynor.board_back.entity;
 
 import org.hibernate.annotations.Tables;
 
+import com.jiraynor.board_back.dto.request.auth.SignUpRequestDto;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -20,5 +22,12 @@ public class UserEntity {
     private String email;
     private String password;
     private String nickname;
+
+    public UserEntity(SignUpRequestDto dto) {
+
+        this.email = dto.getEmail();
+        this.password = dto.getPassword();
+        this.nickname = dto.getNickname();
+    }
 
 }
