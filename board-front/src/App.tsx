d8a701802c742e91c4dfdae4ed20faf01e0caae4
jiwoo1020 <dlwldn041020@gmail.com>
2024-11-08 22:45:28 +0900

    12번 36분까지

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import BoardItem from 'components/BoardItem'; // 경로 확인
+import BoardList from 'components/BoardList'; // 경로 확인
 import { latestBoardListMock } from 'mocks';
-import Top3Item from 'components/Top3Item';
 
 function App() {
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'center', gap: '24px' }}>
-                <Top3Item />
-                <Top3Item />
-                <Top3Item />
-            </div>
+            <BoardList />
         </>
     );
 }
