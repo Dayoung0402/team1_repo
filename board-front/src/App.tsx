5cfe210ea98be38bdd693afbf49f0e94130c4347
jiwoo1020 <dlwldn041020@gmail.com>
2024-11-16 18:12:25 +0900

    save

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -4,11 +4,22 @@ import Main from 'views/Main'; // Main 컴포넌트를 import
 import Authentication from 'views/Authentication';
 import InputBox from 'components/InputBox'
 
+<<<<<<< HEAD
 
 function App() {
     return (
         <InputBox />
     );
+=======
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      {latestBoardListMock.map((boardListItem, index) => (
+        <BoardItem key={index} boardlistItem={boardListItem} />
+      ))}
+    </div>
+  );
+>>>>>>> e218f8a (save)
 }
 
 export default App;
