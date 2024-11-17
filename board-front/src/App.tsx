5c7c3083b5542fd82ddacbe50f7da8bf389f9055
jiwoo1020 <dlwldn041020@gmail.com>
2024-11-18 03:12:04 +0900

    save

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import './App.css';
 import Main from 'views/Main'; // Main 컴포넌트를 import
 import Authentication from 'views/Authentication';
-import InputBox from 'components/InputBox'
-
 
 function App() {
     return (
-        <InputBox />
+        <Authentication />
     );
 }
 
