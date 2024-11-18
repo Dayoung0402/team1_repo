94deaf5026abefe170ba3b41ac5b17a37e9d6d1e
KimJuHui <juhui4078@hufs.ac.kr>
2024-11-19 03:03:19 +0900

    paginatoin

--- board-front/src/views/RecipeBoard/RecipeBoardList/index.tsx
+++ board-front/src/views/RecipeBoard/RecipeBoardList/index.tsx
@@ -4,6 +4,7 @@ import Pagination from 'components/Pagination';
 import BoardItem from 'components/BoardItem';
 import { BoardListItem } from 'types/interface';
 import { latestBoardListMock, top3BoardListMock } from 'mocks';
+import { usePagination } from 'hooks';
 
 
 //          component: 레시피 리스트 게시판 화면 컴포넌트         //
@@ -20,22 +21,33 @@ export default function RecipeBoardList() {
       setCurrentBoardList(latestBoardListMock);
     }, []);
 
+    const { 
+      currentPage, currentSection, viewList, viewPageList, totalSection,
+      setCurrentPage, setCurrentSection, setTotalList
+  } = usePagination<BoardListItem>(3);
+
+    useEffect(() => {
+      // 목데이터를 페이지네이션 훅에 설정
+      setTotalList(latestBoardListMock);
+    }, []);
+
     //          render: 게시물 리스트 게시판 상단 컴포넌트 렌데링          //
     return (
       <div id = 'recipe-board-list-top-wrapper'>
         <div className = 'recipe-board-list-current-contents'>
-          {currentBoardList.map(boardListItem => <BoardItem boardlistItem={boardListItem} />)}
+        {viewList.map((boardListItem) => (
+  <BoardItem key={boardListItem.boardNumber} boardlistItem={boardListItem} />
+))}
         </div>
-      </div>
-    )
-  }
-  //          component: 게시물 리스트 게시판 하단 컴포넌트          //
-  const RecipeBoardListBottom = () =>  {
-    //          render: 게시물 리스트 게시판 상단 컴포넌트 렌데링          //
-    return (
-      <div id = 'recipe-board-list-bottom-wrapper'>
         <div className = 'recipe-board-list-bottom-pagination-box'>
-          <Pagination />
+          <Pagination
+            currentPage={currentPage}
+            currentSection={currentSection}
+            setCurrentPage={setCurrentPage}
+            setCurrentSection={setCurrentSection}
+            viewPageList={viewPageList}
+            totalSection={totalSection}
+          />
         </div>
       </div>
     )
@@ -47,16 +59,8 @@ export default function RecipeBoardList() {
     <div id = 'recipe-board-list-wrapper'>
       <div className = 'recipe-board-list-container'>
         <RecipeBoardListTop />
-        <RecipeBoardListBottom />
       </div>
     </div>
   )
 
 }
-
-
-
-
-
-
-
